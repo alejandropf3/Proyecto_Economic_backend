2247f97ccf7026794286308589a511bfd8b25c66
Julian Diaz <torresanibal336@gmail.com>
2026-02-03 10:57:50 -0500

    Se creo la estructura basica del backend y se realizo la conexion con la base de datos en MySQL

---
+++ Economic/src/main/java/com/backend/economic/Economic.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.backend.economic;
+
+import com.backend.economic.config.ConfigDB;
+
+/**
+ *
+ * @author Propietario
+ */
+public class Economic {
+
+    public static void main(String[] args) {
+        ConfigDB.getConnection();
+    }
+}
---
+++ Economic/src/main/java/com/backend/economic/config/ConfigDB.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.backend.economic.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Propietario
+ */
+public class ConfigDB{
+    private static String URL = "jdbc:mysql://localhost:3306/economic";
+    private static String USER = "root";
+    private static String PASSWD = "#Aprendiz2024";
+
+    public static Connection getConnection(){
+        
+        try
+        {
+             Connection connection = DriverManager.getConnection(URL, USER, PASSWD); 
+             System.out.println("Conexion establecida");
+               return connection;
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+}
