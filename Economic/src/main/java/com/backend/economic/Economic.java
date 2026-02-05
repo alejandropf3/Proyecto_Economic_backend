2030a0d9361e73da8245f7f11e47df3181570116
alejandropf3 <alejandropefa31@gmail.com>
2026-02-05 08:19:04 -0500

    Creamos la estructura de ejecucion del servidor dentro del archivo main

--- Economic/src/main/java/com/backend/economic/Economic.java
+++ Economic/src/main/java/com/backend/economic/Economic.java
@@ -5,6 +5,7 @@
 package com.backend.economic;
 
 import com.backend.economic.config.ConfigDB;
+import com.backend.economic.server.AppServer;
 
 /**
  *
@@ -14,5 +15,13 @@ public class Economic {
 
     public static void main(String[] args) {
         ConfigDB.getConnection();
+
+        try {
+            // Iniciamos el backend
+            AppServer.start(8080);
+        } catch (Exception e) {
+            // Recibimos el error e imprimimos un mensaje
+            System.err.println("Error al arrancar la aplicacion:" + e.getMessage());
+        }
     }
 }
